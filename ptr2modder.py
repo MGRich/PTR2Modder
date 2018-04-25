72d5efb793ca41f4b6c2329bf7789c3227ba6d93
MGRich <29069561+MGRich@users.noreply.github.com>
2018-04-25 00:03:31 -0500

    finish with 2 (partially untested)

--- ptr2modder.py
+++ ptr2modder.py
@@ -18,6 +18,7 @@ if not os.path.isdir("config"):
     os.mkdir("mods")
     os.mkdir("config")
     os.chdir("config")
+    os.mkdir("mds")
     con1 = open("basic.conf", 'w+')
     os.mkdir("ibrn")
     while not os.path.isfile("ibrn/imgburn.exe"):
@@ -71,7 +72,8 @@ if not os.path.isdir("config"):
     time.sleep(5)
     exit()
 else:
-    conf = open("config/basic.conf")
+    conf = os.getcwd() + "\\config\\basic.conf"
+    ibrn = os.getcwd() + "\\config\\ibrn\\imgburn.exe"
     if os.path.isfile("config/create.conf"):
         confc = open("config/create.conf")
 
@@ -114,7 +116,7 @@ while True:
         h2 = str(raw_input("Author> "))
         h3 = str(raw_input("Version> "))
         h4 = str(raw_input("Description> "))
-        m.write(h1 + "\n" + h2 + "\n" + h3 + "\n" + h4 + "\n"   )
+        m.write(h1 + "\n" + h2 + "\n" + h3 + "\n" + h4 + "\n")
         print("Please place your modified ISO contents (only the modified files) WITH THEIR CORRECT FOLDERS in the new 'iso' folder.\nMake sure you do it correctly, as if you mess it up, the mod wont work unless you start over.")
         os.mkdir("iso")
         os.system("pause")
@@ -125,6 +127,7 @@ while True:
                     h = h[2:]
                     mm.append(h)
         m.write(str(mm))
+        m.write(str(linecache.getline(conf, 1)))
         m.close()
         os.chdir("../../config")
         mm = open("mods.conf", "w+")
@@ -155,9 +158,25 @@ while True:
                 os.system("pause")
             else:
                 break
-        hhh = mdl.index(m)
-        os.chdir(mdlo[int(hhh)])
-
+        blo = mdl.index(m)
+        ate = mdlo[int(blo)]
+        os.chdir(ate)
+        pips = linecache.getline("mod.inf", 6)
+        if not pips == str(linecache.getline(conf, 1)):
+            print("This mod is not meant for your region.")
+        else:
+            os.chdir("iso")
+            dc = os.getcwd()
+            print(dc)
+            os.chdir("../../..")
+            print("Applying mod..")
+            os.system("xcopy /s /q /y " + dc + " miso")
+            print("Done with general modding. Starting with IMGBurn..")
+            os.system("move " + ibrn + ".")
+            os.system("imgburn.exe /MODE BUILD /SRC miso /DEST PTR2Modded.iso /FILESYSTEM \"ISO9660 + UDF \" /UDFREVISION \"1.02\" /NOIMAGEDETAILS /ROOTFOLDER YES /VOLUMELABEL \"MISO\" /OVERWRITE YES /START /CLOSE")
+            os.system("move imgburn.exe config/ibrn")
+            print("Done! Exported to PTR2Modded.iso.")
+        os.system("pause")
     elif ch == "4":
         print(open("temp/news.txt").read())
         os.system("pause")
