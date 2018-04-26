2c61a01cf72451e8f442c1a932d1ab5222bb6eeb
MGRich <29069561+MGRich@users.noreply.github.com>
2018-04-25 22:34:56 -0500

    why do i keep messing stuff u p

--- update.py
+++ update.py
@@ -5,7 +5,7 @@ from decimal import Decimal as decimal
 v = open("temp/version.txt").read()
 v = float(v[:-1])
 print("Downloading..")
-urllib.urlretrieve("https://github.com/MGRich/ptr2modder/releases/download/" + str(v) + "b" "/ptr2modder.exe ptr2modder.exe    ")
+urllib.urlretrieve("https://github.com/MGRich/ptr2modder/releases/download/" + str(v) + "b" "/ptr2modder.exe ptr2modder.exe")
 print("Starting..")
 subprocess.Popen("ptr2modder.exe")
 exit()
