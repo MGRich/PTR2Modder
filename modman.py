bb2f4cf696deabddfa47d032c5519bc7d68434cf
MGRich <29069561+MGRich@users.noreply.github.com>
2018-05-10 19:47:50 -0500

    nother checkpoint

--- modman.py
+++ modman.py
@@ -21,7 +21,13 @@ while True:
     else:
         break
 mnam = m
-ms = raw_input("What is your MINGW64 PTR2Tools directory?\n(i didnt bother to put this in config due to pain)\n(msysinstall)/mingw64/bin (KEEP SLASH AT END)> ")
+try:
+    ms = open(tls[:-5] + "modman.conf").readlines()[0]
+except:
+    ms = raw_input("What is your MINGW64 PTR2Tools directory?\n(This will be the only time you are asked this.)\n(msysinstall)/mingw64/bin (KEEP SLASH AT END)> ")
+    with open(tls[:-5] + "modman.conf", "w+") as r:
+        r.write(ms)
+    ms = open(tls[:-5] + "modman.conf").readlines()[0]
 n = False
 x = 0
 #HERE WE GOOOOOOOO
@@ -61,19 +67,27 @@ while True:
                 print("I. Extract INT")
                 op.append("i")
             if os.path.split(os.getcwd())[-1] == "SND":
-                print("C. Convert Music")
+                print("C. Convert Music (ONLY STAGES WP2)")
                 op.append("c")
-            if os.path.split(os.getcwd())[-1][:3] == "st0":
+            if os.path.split(os.getcwd())[-1][:3] == "st0" and not os.path.split(os.getcwd())[0][-3:] == "SND":
                 print("P. Re-pack INT")
                 op.append("p")
-            if os.path.split(os.getcwd())[-1][:3] == "st0" and not os.path.isdir("tex"):
+            if os.path.split(os.getcwd())[-1][:3] == "st0" and os.path.split(os.getcwd())[0][-3:] == "SND":
+                print("M. Re-build Music")
+                op.append("m")
+            if os.path.split(os.getcwd())[-1][:3] == "st0" and not os.path.split(os.getcwd())[0][-3:] == "SND":
                 print("T. Extract Textures")
                 op.append("t")
-            if os.path.split(os.getcwd())[-1][:3] == "st0" and os.path.isdir("tex"):
+            if os.path.split(os.getcwd())[-1][:3] == "st0" and os.path.isdir("tex") and not os.path.split(os.getcwd())[0][-3:] == "SND":
                 print("R. Inject Textures")
                 op.append("r")
             break
         opc = ord(getch())
+        if opc == 8:
+            os.chdir("..")
+            if "iso" in os.listdir("."):
+                os.chdir("iso")
+            x = 0
         if opc == 13:
             if os.path.isdir(dir[x][1:]):
                 os.chdir(dir[x][1:])
@@ -118,11 +132,13 @@ while True:
                         print os.getcwd()
                         os.system("pause")
                         mm = []
-                        for root, directories, filenames in os.walk('.'):
-                            for filename in filenames: 
-                                    h = os.path.join(root,filename)
-                                    h = h[2:]
-                                    mm.append(h)
+                        fldrs = [j for j in os.listdir(".") if os.path.isdir(os.path.join(".", j))]
+                        for f in fldrs:
+                            os.chdir(f)
+                            for t in [j for j in os.listdir(".") if os.path.isfile(os.path.join(".", j))]:
+                                t = f + "\\" + t
+                                mm.append(t)
+                            os.chdir("..")
                         lmfao = []
                         dect = str(mm).lower()
                         for u in range(1,9):
@@ -163,6 +179,7 @@ while True:
                         mdin[7] = str(charx)
                         md.truncate()
                         md.close()
+                        os.chdir("..")
                         md = open("mod.inf", "r+")
                         md.writelines(mdin)
                         md.close()
@@ -172,22 +189,47 @@ while True:
                 dch = raw_input("Name of INT (with .int)> ")
                 print("Working..")
                 os.system(ms + "ptr2int.exe e "+ og + "\\DATA\\" + dch + " " + dch[:-4] + ">nul")
-            if opc == "p":
+            elif opc == "c":
+                dch = raw_input("WP2 Name (with .wp2)> ")
+                print("Working..")
+                fdch = dch[:-5]
+                os.mkdir(fdch)
+                os.system(tls + "\\ptr2mus.bat " + og + "\\SND\\" + dch + ">nul")
+                os.system("pause")
+                os.system("xcopy " + tls + "\\wp2 " + os.getcwd() + "\\" + fdch)
+            elif opc == "p":
                 print("Working..")
                 dch = os.path.split(os.getcwd())[-1] + ".int"
                 os.chdir("..")
                 os.system(ms + "ptr2int.exe c "+ dch + " " + dch[:-4] + ">nul")            
                 os.chdir(dch[:-4])
+            elif opc == "m":
+                print("Working..")
+                fdch = os.path.split(os.getcwd())[-1]
+                os.chdir("..")
+                os.system("ren " + fdch + " wp2")
+                print("Please follow the prompt correctly (use WP2)\n------\n")
+                os.system(tls + "\\ptr2mus.bat")
+                print("The files are in now placed in the folder the WAVs are.\nPlease rename them accordingly for use.")
+                os.system("ren wp2 " + fdch)
+                os.system("xcopy /q /y " + tls + "\\wp2 " + fdch)
+                os.chdir(fdch)
+                os.system("pause")           
             elif opc == "t":
                 dch = raw_input("Name of Model (with .spm)> ")
                 os.chdir("PROPS")
-                os.system(ms + "ptr2spm.exe gtex0 "+ dch + " temp" + ">nul")
-                shutil.move("temp", "../temp")
+                arg = raw_input("What should the folder name then tex0 file name be? (separate by comma and space, please do in order)")
+                arg = arg.split(", ")
+                os.system(ms + "ptr2spm.exe gtex0 "+ dch + " " + arg[1] + ">nul")
+                shutil.move(arg[1], "../" + arg[1])
                 os.chdir("..")
-                os.mkdir("tex")
-                os.system(ms + "ptr2tex.exe e TEXTURES temp tex" + ">nul")
+                if not os.path.isdir("tex"):
+                    os.mkdir("tex")
+                os.system(ms + "ptr2tex.exe e TEXTURES " + arg[1] + " " + arg[0] + ">nul")
             elif opc == "r":
-                os.system(ms + "ptr2tex.exe i TEXTURES temp tex" + ">nul")
+                arg = raw_input("What is folder name then tex0 file name be? (separate by comma and space, please do in order)")
+                arg = arg.split(", ")
+                os.system(ms + "ptr2tex.exe i TEXTURES " + arg[1] + " " + arg[0] + ">nul")
         elif opc == 27:
             n = True
         break
